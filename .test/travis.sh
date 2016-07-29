f78429575c865139b455b1f95e347c1a0e59faf3
Tony Kelman <tony@kelman.net>
2016-07-29 07:17:24 -0400

    don't run nightly until it becomes 0.6

--- .test/travis.sh
+++ .test/travis.sh
@@ -11,11 +11,11 @@ for ver in 0.4 0.5; do # releases
     tar -C julia-$ver --strip-components=1 -xzf - && \
     julia-$ver/bin/julia -e 'versioninfo(); include("METADATA/.test/METADATA.jl")' || exit 1 &
 done
-# nightly, future proofing a little
+# nightly, uncomment this once julia master is 0.6-dev
 ver=0.6
 mkdir -p ~/.julia/v$ver julia-$ver
 ln -s $PWD/METADATA.jl ~/.julia/v$ver/METADATA
-curl -L --retry 5 https://s3.amazonaws.com/julianightlies/bin/linux/x64/julia-latest-linux64.tar.gz | \
-  tar -C julia-$ver --strip-components=1 -xzf - && \
-  julia-$ver/bin/julia -e 'versioninfo(); include("METADATA/.test/METADATA.jl")' || exit 1 &
+#curl -L --retry 5 https://s3.amazonaws.com/julianightlies/bin/linux/x64/julia-latest-linux64.tar.gz | \
+#  tar -C julia-$ver --strip-components=1 -xzf - && \
+#  julia-$ver/bin/julia -e 'versioninfo(); include("METADATA/.test/METADATA.jl")' || exit 1 &
 wait
